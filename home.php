66137dc7d098f644c2ad5d789b83e610a6d89272
Nazmul Hoque <hoque23kazi@gmail.com>
2022-06-11 19:39:00 +0600

    fixed bug in home.php

--- home.php
+++ home.php
@@ -19,10 +19,10 @@
 			  $skuData = $dbTable->getSku();
 			  // count($skuData);
 			  $i = 0;?>
-			   <form class = "checkClass" method="post" action = "">	
+			   <form class = "checkClass" method="get" action = "">	
 			  		<div class = "header">
 					<h1>Product List</h1>
-					<button >Add</button>
+					<button type="submit" formaction="form.php">Add</button>
 					<button type="submit"  name = "save" id = "delete" value="delete" >Delete</button>
 					</div>
 			  		<?php foreach($data as $data1):?>
@@ -52,16 +52,16 @@
 		
 		<?php 
 		
-		$checked =(isset($_POST['myCheck']))? $_POST['myCheck']:""; 
+		$checked =(isset($_GET['myCheck']))? $_GET['myCheck']:""; 
 		
 		$valCount = !empty($checked)? count($checked,0):""; 
 		
 		for($i = 0; $i<$valCount; $i++){
 			//$del_id = $valCount[$i];
-			$sql = "DELETE FROM `products` WHERE SKU = '" . $_POST['myCheck'][$i] . "'";
+			$sql = "DELETE FROM `products` WHERE SKU = '" . $_GET['myCheck'][$i] . "'";
 		
 			$deleteData = $dbTable->connect()->exec($sql);	
-			header("location:home.php");	
+			header("location:home.php");		
 		}
 		
 
